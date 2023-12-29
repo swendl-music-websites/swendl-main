db52aa20531b971fb7b9a53333bf43e340208c91
swendlcode <swendlmusic@gmail.com>
2023-12-28 19:03:07 -0500

    New edit: Speed of Light

--- js/utils/redirect.js
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@lib/utils'; // Import the utility function to handle redirection
-
-export default async function handler(req, res) {
-  // Redirect all requests to the main page
-  await redirect('/', res);
-}
